d3b1e367ee81b3ba8094e9283e0046deed0305fe
patapun <156818433+patapun@users.noreply.github.com>
2024-01-24 13:56:55 +0000

    Update countcase.py

--- countcase.py
+++ countcase.py
@@ -5,6 +5,12 @@ The function should calculate and return two numbers: the count of uppercase let
 If there are no letters of a particular case (uppercase or lowercase) in the string, the function should return 0 for that case.
 Non-alphabetic characters in the string should be ignored and not counted.
 """
+#Dont forget def is the main, you have to inline print at inner paragraph
+#s here is string
+#d is what we determine to start counting case at 0
+#for c in s means for case in string
+#c.isupper is function text.isupper 
+# d["UPPER_CASE"] += 1 mean UPPER_CASE = 0+1
 
 def case_counter(s):
     d = {"UPPER_CASE": 0,"LOWER_CASE": 0}
@@ -24,4 +30,3 @@ print(case_counter("PYTHON"))
 print(case_counter("python"))
 print(case_counter("1234!@#$"))
 print(case_counter("Pata DONT BE SCARE of Python"))
-        
