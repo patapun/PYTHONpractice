bf8f620725afc6496819b597b9e1d6410881ca48
patapun <156818433+patapun@users.noreply.github.com>
2024-01-24 13:52:32 +0000

    Add files via upload

---
+++ countcase.py
@@ -0,0 +1,27 @@
+"""
+Write a Python function named case_counter that counts the number of uppercase and lowercase letters in a given string.
+
+The function should calculate and return two numbers: the count of uppercase letters and the count of lowercase letters in the string.
+If there are no letters of a particular case (uppercase or lowercase) in the string, the function should return 0 for that case.
+Non-alphabetic characters in the string should be ignored and not counted.
+"""
+
+def case_counter(s):
+    d = {"UPPER_CASE": 0,"LOWER_CASE": 0}
+    for c in s:
+        if c.isupper():
+            d["UPPER_CASE"] += 1
+        if c.islower():
+            d["LOWER_CASE"] += 1
+
+        
+    print("Original String", s)
+    print("Number of Upper case character: ",d["UPPER_CASE"])
+    print("Number of Lower case character: ",d["LOWER_CASE"])
+
+print(case_counter("Hello World!"))
+print(case_counter("PYTHON"))
+print(case_counter("python"))
+print(case_counter("1234!@#$"))
+print(case_counter("Pata DONT BE SCARE of Python"))
+        
